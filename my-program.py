338fc3181b3ad9a07bfb19046084d5662fa910ea
Hoang Long <litpuvn@gmail.com>
2017-11-23 15:35:46 -0600

    update my-program

--- my-program.py
+++ my-program.py
@@ -14,18 +14,28 @@ def do_integer_multiplication(a, b):
         b = 1
 
     # code mitigate a flaw made by programmer
-    if a > 100:
+    if a > 1000:
+        c = 100
+
+    if a > 100 and a < 1000:
         a = a + 2
-    if a > 50:
-        a = a + 4
-    if a > 20:
+
+    if a > 50 and a < 100:
+        c = 100
+
+    if a > 20 and a < 50:
         a = a + 6
 
-    if b > 100:
-        b = b+ 2
-    if b > 50:
-        b = b + 4
-    if b > 20:
+    if b > 1000:
+        c = 100
+
+    if b > 100 and b < 1000:
+        b = b + 2
+
+    if b > 50 and b < 100:
+        c = 10000
+
+    if b > 20 and b < 50:
         b = b + 6
 
     return a*b
