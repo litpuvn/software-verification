bcfb6e4b17487ea3de70f4306c157150b1aef194
Hoang Long <litpuvn@gmail.com>
2017-11-23 23:40:50 -0600

    add measures for svm

--- svm.R
+++ svm.R
@@ -1,7 +1,7 @@
 library("e1071")
 
 
-setwd("/home/long/TTU-SOURCES/software-verification")
+setwd("/home/hoanglong/TTU-SOURCES/software-verification")
 
 training <- read.csv("trace-training.csv", header = TRUE)
 
@@ -23,8 +23,18 @@ summary(svm_model)
 testdat = data.frame(training[(row_split_train_and_test+1):nrow(training), ])
 testdat$mul = as.factor(testdat$mul)
 
-ypred = predict(svm_model, testdat)
+pred = predict(svm_model, testdat)
 
-table(predict = ypred , truth = testdat$mul)
+confusionMatrix = table(pred, testdat$mul)
 
+a = confusionMatrix[2,2]
+b = confusionMatrix[2,1]
+c = confusionMatrix[1, 2]
+d = confusionMatrix[1, 1]
 
+precision = a / (a + c)
+recall = a / (a + b)
+fMeasure = 2*a / (2*a + b + c)
+accuracy = (a + d) / (a + b + c +d)
+
+message(paste("accuracy: ", accuracy, "; precision: ", precision, "; recall: ", recall, "; f-measure: ", fMeasure))
